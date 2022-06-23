94e90a404ec97743a68df9ad39a5a4fe94f212ad
prissila <92823591+prissila-fabian@users.noreply.github.com>
2022-06-22 18:03:47 -0700

    My Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,10 +18,9 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Prissila Fabian</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hello! I am a Senior at California State Northridge studying Computer Science and Computer Mathematics.
             </div>
           </div>
         </div>
